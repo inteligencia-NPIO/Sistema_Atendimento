08d0f006ad4b0a28d537a351c15bd4dc30981e53
inteligencia-NPIO <intenegociosunpio117@gmail.com>
2026-01-19 11:11:50 -0300

    Add files via upload

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,3 +1,9 @@
-const API_URL = import.meta.env.PROD ? '' : 'http://127.0.0.1:8000';
+let API_URL = '';
+
+if (window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1') {
+  API_URL = 'http://127.0.0.1:8000';
+} else {
+  API_URL = '';
+}
 
 export default API_URL;
